1a4d8a13118ccad044ff1cb92cd83d1200f601b6
unknown <contato.itamargouveia@gmail.com>
2022-01-10 19:44:17 -0300

    search prefix term

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const readline = require('readline-sync');
 function start(){
     const content ={}
     content.searchTerm = askAndReturnSearchTerm();
+    content.prefix = askAndReturnPrefix();
+
 
     function askAndReturnSearchTerm(){
         return readline.question("Type a search term: ");
+    }
+
+    function askAndReturnPrefix(){
+        const prefixes = ['who is', 'what is', 'history'];
+        const selectedPrefixIndex = readline.keyInSelect(prefixes,"Choose a option");
+        const selectedPrefixText = prefixes[selectedPrefixIndex];
+
+        return selectedPrefixText;
     }
 
     console.log(content);
